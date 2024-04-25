462115fbb223ae0395ddd6411d3a1de41e93a18d
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-25 14:34:20 +0000

    Задается и печатается время взлета и посадки

---
+++ src/main/kotlin/lesson1/lesson-1-task-3.kt
@@ -0,0 +1,11 @@
+package org.example.lesson1
+
+fun lesson_1_task_3() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("$year\n$hour\n$minute")
+    hour = 10
+    minute = 55
+    print("$hour:$minute")
+}
