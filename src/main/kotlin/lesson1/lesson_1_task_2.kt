1393254e0b50d1120f0df1765c0d585f02a4d8b2
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-25 13:24:19 +0000

    Распечатаны переменные, добавлена переменная для количества работников, проинициализирована, уменьшана и распечатана

---
+++ src/main/kotlin/lesson1/lesson_1_task_2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun lesson_1_task_2() {
+    val orderNumber = 75
+    val greetingMessage = "Спасибо, что выбираете наш интернет-магазин"
+
+    println("Number of orders $orderNumber")
+    println(greetingMessage)
+
+    var workersCount = 2000
+    //println("Workers count $workersCount")
+    workersCount--
+    println("Workers count $workersCount")
+}
