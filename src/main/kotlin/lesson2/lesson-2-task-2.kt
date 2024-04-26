ec0f22a0b32328c6349d4923fb9428e0b3f8e4f3
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-26 09:16:28 +0000

    Средний балл учеников

---
+++ src/main/kotlin/lesson2/lesson-2-task-2.kt
@@ -0,0 +1,12 @@
+package lesson2
+
+fun main() {
+    val mark1 = 3
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+
+    val avg : Float = (mark1 + mark2 + mark3 + mark4) / 4f
+
+    println("Average mark: %.2f".format(avg))
+}
