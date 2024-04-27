4ea6456cdccf87f9c70f3e9fb39f96d082ea3a01
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-27 08:13:17 +0000

    Приветствие

---
+++ src/main/kotlin/lesson3/lesson-3-task-1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson3
+
+fun main() {
+
+    var greeting = "Good afternoon!"
+    val userName = "Alexander"
+
+    println("$greeting $userName")
+    greeting = "Good evening!"
+    println("$greeting $userName")
+}
