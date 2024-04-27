f5fbd7f7428752eebbd54df5aa3155a109dc2493
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-27 08:33:13 +0000

    Таблица умножения

---
+++ src/main/kotlin/lesson3/lesson-3-task-3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson3
+
+fun main() {
+    val factor = 4
+
+    println("""
+        $factor x 1 = $factor
+        $factor x 2 = ${factor * 2}
+        $factor x 3 = ${factor * 3}
+        $factor x 4 = ${factor * 4}
+        $factor x 5 = ${factor * 5}
+        $factor x 6 = ${factor * 6}
+        $factor x 7 = ${factor * 7}
+        $factor x 8 = ${factor * 8}
+        $factor x 9 = ${factor * 9}
+    """.trimIndent())
+}
