6c0853c8d31efe1fdb83327a80735d4e1e199a35
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-28 14:26:35 +0000

    start task

---
+++ src/main/kotlin/lesson4/lesson-4-task-4.kt
@@ -0,0 +1,11 @@
+package lesson4
+
+fun main() {
+    val set1 = "руки/пресс"
+    val set2 = "ноги/спина"
+
+    val startSet = set1
+    val currentDay = 5
+
+    var currentSet =  currentDay % 2
+}
