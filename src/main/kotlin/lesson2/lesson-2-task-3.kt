cf83b0e7d800339a377582ddc0b2cde68320faba
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-28 14:28:20 +0000

    Удалены магические чила

--- src/main/kotlin/lesson2/lesson-2-task-3.kt
+++ src/main/kotlin/lesson2/lesson-2-task-3.kt
@@ -6,11 +6,14 @@ fun main() {
     val hour = 9
     val minute = 39
 
-    val departureTimeInMinutes = hour * 60 + minute
+    val minutesInHour = 60
+    val hoursInDay = 24
+
+    val departureTimeInMinutes = hour * minutesInHour + minute
     val arriveTimeInMinutes = departureTimeInMinutes + wayTime
 
-    val arriveMinute = arriveTimeInMinutes % 60
-    val arriveHour = (arriveTimeInMinutes / 60) % 24
+    val arriveMinute = arriveTimeInMinutes % minutesInHour
+    val arriveHour = (arriveTimeInMinutes / minutesInHour) % hoursInDay
 
     println("Arrive time: %02d:%02d".format(arriveHour, arriveMinute))
 }
