64f9ee9ba350fd2d41a3c38211791cf5843c7161
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-30 07:53:07 +0000

    Проверка возраста

---
+++ src/main/kotlin/lesson5/lesson-5-task-2.kt
@@ -0,0 +1,17 @@
+package lesson5
+
+import java.time.LocalDate
+
+const val AGE_OF_MAJORITY = 18
+
+fun main() {
+
+    val currentYear = LocalDate.now().year
+
+    println("Введите год своего рожедения:")
+    val yearOfBirth = readlnOrNull()?.toInt()
+    if (yearOfBirth != null && currentYear - yearOfBirth >= AGE_OF_MAJORITY)
+        println("Показать экран со скрытым контентом")
+    else
+        println("Вернуть пользователя на главный экран.")
+}
