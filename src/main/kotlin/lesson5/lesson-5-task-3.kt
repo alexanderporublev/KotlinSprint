a365d96a641c11af68a93ad8d23e7264943cb4cf
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-30 12:34:22 +0000

    Учитываем обратный порядок

--- src/main/kotlin/lesson5/lesson-5-task-3.kt
+++ src/main/kotlin/lesson5/lesson-5-task-3.kt
@@ -10,9 +10,12 @@ fun main() {
     println("Введите второе число:")
     val secondGuessedNumber = readlnOrNull()?.toInt()
 
-    if (firstGuessedNumber != null && secondGuessedNumber != null && firstGuessedNumber == firstNumber && secondGuessedNumber == secondNumber)
+    val firstGuessed = firstGuessedNumber != null && firstGuessedNumber == firstNumber || secondGuessedNumber != null && secondGuessedNumber == firstNumber
+    val secondGuessed = firstGuessedNumber != null && firstGuessedNumber == secondNumber || secondGuessedNumber != null && secondGuessedNumber == secondNumber
+
+    if (firstGuessed && secondGuessed)
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (firstGuessedNumber != null && firstGuessedNumber == firstNumber || secondGuessedNumber != null && secondGuessedNumber == secondNumber)
+    else if (firstGuessed || secondGuessed)
         println("Вы выиграли утешительный приз!")
     else
         println("Неудача!")
