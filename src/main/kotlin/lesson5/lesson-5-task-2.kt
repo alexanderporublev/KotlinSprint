3ccf8791cbae7d28eb0608ce23e24aac15402d1f
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-04-30 07:53:32 +0000

    форматирование

--- src/main/kotlin/lesson5/lesson-5-task-2.kt
+++ src/main/kotlin/lesson5/lesson-5-task-2.kt
@@ -5,11 +5,11 @@ import java.time.LocalDate
 const val AGE_OF_MAJORITY = 18
 
 fun main() {
-
     val currentYear = LocalDate.now().year
 
     println("Введите год своего рожедения:")
     val yearOfBirth = readlnOrNull()?.toInt()
+
     if (yearOfBirth != null && currentYear - yearOfBirth >= AGE_OF_MAJORITY)
         println("Показать экран со скрытым контентом")
     else
