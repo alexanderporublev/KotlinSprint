281854158c14846696dd2575dc233448794938d6
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-02 07:29:56 +0000

    Вынес параметры стандартного груза в константу

--- src/main/kotlin/lesson4/lesson-4-task-2.kt
+++ src/main/kotlin/lesson4/lesson-4-task-2.kt
@@ -1,15 +1,15 @@
 package lesson4
 
-fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+const val MIN_WEIGHT = 35
+const val MAX_WIGHT = 100
+const val MAX_VOLUME = 100
 
+fun main() {
     val weightVariant1 = 20
     val volumeVariant1 = 80
-    println("Соответствие категории Average: ${weightVariant1 >= minWeight && weightVariant1 <= maxWeight && volumeVariant1 < maxVolume}")
+    println("Соответствие категории Average: ${weightVariant1 >= MIN_WEIGHT && weightVariant1 <= MAX_WIGHT && volumeVariant1 < MAX_VOLUME}")
 
     val weightVariant2 = 50
     val volumeVariant2 = 100
-    println("Соответствие категории Average: ${weightVariant2 >= minWeight && weightVariant2 <= maxWeight && volumeVariant2 < maxVolume}")
+    println("Соответствие категории Average: ${weightVariant2 >= MIN_WEIGHT && weightVariant2 <= MAX_WIGHT && volumeVariant2 < MAX_VOLUME}")
 }
