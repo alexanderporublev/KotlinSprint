a955df21b3e004084eb9f0ffe3e361b8b0e768f0
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-02 07:21:37 +0000

    Вынес количество столиков в константу

--- src/main/kotlin/lesson4/lesson-4-task-1.kt
+++ src/main/kotlin/lesson4/lesson-4-task-1.kt
@@ -1,10 +1,11 @@
 package lesson4
 
+const val TOTAL_TABLE_AMOUNT = 13
+
 fun main() {
-    val totalTableAmount = 13
     val busyTablesToday = 13
     val busyTableTomorrow = 9
 
-    println("Доступность столиков на сегодня: ${busyTablesToday < totalTableAmount}")
-    println("Доступность столиков на завтра: ${busyTableTomorrow < totalTableAmount}")
+    println("Доступность столиков на сегодня: ${busyTablesToday < TOTAL_TABLE_AMOUNT}")
+    println("Доступность столиков на завтра: ${busyTableTomorrow < TOTAL_TABLE_AMOUNT}")
 }
