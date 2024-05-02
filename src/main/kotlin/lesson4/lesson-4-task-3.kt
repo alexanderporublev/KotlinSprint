97f67deec46b579b3e4c2c969b823f082e0309c2
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-02 07:33:41 +0000

    Вынес параметры в константу Время года задано строкой

--- src/main/kotlin/lesson4/lesson-4-task-3.kt
+++ src/main/kotlin/lesson4/lesson-4-task-3.kt
@@ -1,10 +1,13 @@
 package lesson4
 
+const val COMFORT_HUMIDITY = 20
+const val WINTER = "winter"
+
 fun main() {
     val weatherIsSunny = true
     val humidity = 20
     val tentIsOpened = true
-    val winterIsNow = true
+    val timeOfYear = WINTER
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${weatherIsSunny && tentIsOpened && !winterIsNow && humidity == 20}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${weatherIsSunny && tentIsOpened && timeOfYear == WINTER && humidity == COMFORT_HUMIDITY}")
 }
