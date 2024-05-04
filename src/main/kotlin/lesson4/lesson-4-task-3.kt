d5bfe1fe3f9357349bae175b2c335d2f21cedefc
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-04 06:58:53 +0000

    Замечания

--- src/main/kotlin/lesson4/lesson-4-task-3.kt
+++ src/main/kotlin/lesson4/lesson-4-task-3.kt
@@ -2,13 +2,13 @@ package lesson4
 
 const val COMFORT_HUMIDITY = 20
 const val WINTER = "winter"
-const val COMFORT_WEATHER = "sunny"
-const val COMFORT_TENT_STATE = "opened"
+const val IS_SUNNY = true
+const val IS_AWING_OPEN = true
 fun main() {
-    val weather = "sunny"
+    val isSunny = true
     val humidity = 20
-    val tentState = "opened"
+    val isAwningOpen = true
     val timeOfYear = "winter"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${weather == COMFORT_WEATHER && tentState == COMFORT_TENT_STATE && timeOfYear == WINTER && humidity == COMFORT_HUMIDITY}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${isSunny == IS_SUNNY && isAwningOpen == IS_AWING_OPEN && timeOfYear == WINTER && humidity == COMFORT_HUMIDITY}")
 }
