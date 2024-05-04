4bad928e3a710c536450c1026033d7bce512e522
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-04 07:06:02 +0000

    Замечания

--- src/main/kotlin/lesson5/lesson-5-task-2.kt
+++ src/main/kotlin/lesson5/lesson-5-task-2.kt
@@ -8,9 +8,9 @@ fun main() {
     val currentYear = LocalDate.now().year
 
     println("Введите год своего рожедения:")
-    val yearOfBirth = readlnOrNull()?.toInt()
+    val yearOfBirth = readln().toInt()
 
-    if (yearOfBirth != null && currentYear - yearOfBirth >= AGE_OF_MAJORITY)
+    if (currentYear - yearOfBirth >= AGE_OF_MAJORITY)
         println("Показать экран со скрытым контентом")
     else
         println("Вернуть пользователя на главный экран.")
