d18882dd747ed25bd11a8ffd1ed900726222fe07
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-04 07:04:39 +0000

    Замечания

--- src/main/kotlin/lesson5/lesson-5-task-1.kt
+++ src/main/kotlin/lesson5/lesson-5-task-1.kt
@@ -1,15 +1,15 @@
 package lesson5
 
 fun main() {
-    val first = (Math.random() * 100).toInt()
-    val second = (Math.random() * 100).toInt()
+    val first = (1..100).random()
+    val second = (1..100).random()
 
     println("Докажите, что вы не робот. Решите простой пример.")
     println("$first + $second =")
     println("Введите ваш ответ:")
 
-    val userSum = readlnOrNull()?.toInt()
-    if (userSum != null && userSum == first + second)
+    val userSum = readln().toInt()
+    if (userSum == first + second)
         println("Добро пожаловать!")
     else
         println("Доступ запрещен.")
