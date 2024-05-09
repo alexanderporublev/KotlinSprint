3655ab7c8904d9ab070d30c02a97d7375e83285f
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-09 06:52:53 +0000

    Замечания

--- src/main/kotlin/lesson7/lesson-7-task-2.kt
+++ src/main/kotlin/lesson7/lesson-7-task-2.kt
@@ -1,11 +1,13 @@
 package lesson7
 
 fun main() {
+    var inputCode = 0
+
     do {
         val code = (1000..9999).random()
         println("Ваш код авторизации $code")
         println("Введите код авторизации:")
-        val inputCode = readln().toInt()
+        inputCode = readln().toInt()
     } while (inputCode != code)
 
     println("Добро пожаловать!")
