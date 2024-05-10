a3c96ec865154413f0354a42bdf1fd10904b74d4
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-10 07:00:21 +0000

    Регистрация

---
+++ src/main/kotlin/lesson10/lesson-10-task-2.kt
@@ -0,0 +1,16 @@
+package lesson10
+
+const val MIN_LENGTH = 4
+
+fun isLengthValid(str : String) = str.length >= MIN_LENGTH
+
+fun main() {
+    println("Создайте логин:")
+    val login = readln()
+
+    println("Создайте пароль:")
+    val password = readln()
+
+    if (!isLengthValid(login) || !isLengthValid(password))
+        println("Логин или пароль недостаточно длинные")
+}
