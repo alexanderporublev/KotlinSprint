7c6e2d6fc26ec77e8bf380cf8b465e54d13e0a6a
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-10 07:11:47 +0000

    Генератор паролей

---
+++ src/main/kotlin/lesson10/lesson-10-task-3.kt
@@ -0,0 +1,19 @@
+package lesson10
+
+fun generatePassword(length: Int) : String {
+    val specialSymbols = "!\"#\$%&'()*+,-./ "
+    var password = "" + (0..9).random() + specialSymbols[specialSymbols.indices.random()]
+    while (password.length < length) {
+        password += when ((0..1).random()) {
+            0 -> (0..9).random()
+            1 -> specialSymbols[specialSymbols.indices.random()]
+            else -> 'u'
+        }
+    }
+    return password.toList().shuffled().joinToString("")
+}
+
+fun main() {
+    println("Введите длину пароля:")
+    println("Пароль: ${generatePassword(readln().toInt())}")
+}
