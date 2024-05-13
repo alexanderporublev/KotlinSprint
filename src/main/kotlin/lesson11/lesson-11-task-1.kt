2151e62d44e5f39f4e948ebd2ac0a78c7248f0b5
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-13 04:44:21 +0000

    Распечатка пользователей

---
+++ src/main/kotlin/lesson11/lesson-11-task-1.kt
@@ -0,0 +1,37 @@
+package lesson11
+
+class User(
+    val id: String,
+    val login: String,
+    val password: String,
+    val email: String,
+) {
+    fun print() {
+        println("""
+            id      :   $id
+            login   :   $login
+            password:   $password
+            email   :   $email
+        """.trimIndent())
+    }
+}
+
+fun main() {
+    val user1 = User(
+        id = "1111",
+        login = "lex",
+        password = "qwerty",
+        email = "lex@gmail.com",
+    )
+
+    val user2 = User(
+        id = "1234",
+        login = "angel",
+        password = "lkjuiop",
+        email = "qwerty@yandex.ru",
+    )
+
+    user1.print()
+    println()
+    user2.print()
+}
