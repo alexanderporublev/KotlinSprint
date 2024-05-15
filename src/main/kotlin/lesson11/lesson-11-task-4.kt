76bb4bc2515e4a8a3406f794fa66be4dd21bb504
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-15 10:10:22 +0000

    Рецепты

---
+++ src/main/kotlin/lesson11/lesson-11-task-4.kt
@@ -0,0 +1,24 @@
+package lesson11
+
+class Ingredient(
+    val name: String,
+    val amount: Double,
+    val measureUnit: String,
+) {
+    companion object {
+        const val MEASURE_GRAMM = "Г"
+        const val MEASURE_ITEMS = "ШТУКИ"
+        const val MEASURE_BIG_SPOON = "С. ЛОЖЕК"
+        const val MEASURE_LITTLE_SPOON = "Ч. ЛОЖЕК"
+    }
+}
+
+class Receipt (
+    val name : String,
+    val ingredients : List<Ingredient>,
+)
+
+class Category (
+    val name : String,
+    val receipts: List<Receipt>,
+)
