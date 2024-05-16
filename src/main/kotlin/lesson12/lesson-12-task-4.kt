6df99d412d98d830414339c749dc07aaa2cfa176
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-16 08:19:48 +0000

    Погода с конструктором с параметрами c выводом в консоль при создании

---
+++ src/main/kotlin/lesson12/lesson-12-task-4.kt
@@ -0,0 +1,35 @@
+const val degreeSymbol = '\u00B0'
+
+const val ZERO_ABSOLUTE = 273
+
+fun kelvinToCelsius(temperatureKelvin: Int) = temperatureKelvin - ZERO_ABSOLUTE
+
+class WhetherData3(
+    dayTemperature: Int,
+    nightTemperature: Int,
+    rain: Boolean,
+) {
+    init {
+        print()
+    }
+
+    var dayTemperature = kelvinToCelsius(dayTemperature)
+    var nightTemperature = kelvinToCelsius(nightTemperature)
+    var rain = rain
+
+    fun print() = println(
+        """
+        Дневная температура: ${dayTemperature}${degreeSymbol}C
+        Ночная температура: ${nightTemperature}${degreeSymbol}C
+        Наличие осдаков: ${if (rain) "Были" else "Нет"}
+    """.trimIndent()
+    )
+}
+
+fun main() {
+    val whether1 = WhetherData3(
+        rain = true,
+        dayTemperature = 300,
+        nightTemperature = 290,
+    )
+}
