056cca761dab8c8c908e2d1ff9dcc7de5c4651e1
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-16 12:39:47 +0000

    Обработка ошибок

---
+++ src/main/kotlin/lesson13/lesson-13-task-5.kt
@@ -0,0 +1,43 @@
+package lesson13
+
+const val NOTES_AMOUNT = 3
+
+class Note4(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+    fun print() = println(
+        """
+        Имя: $name
+        Номер: $phoneNumber}
+        Компания: ${company ?: "не указано"}
+    """.trimIndent()
+    )
+}
+
+fun main() {
+    val notes = listOf<Note4>().toMutableList()
+
+    while (notes.size < NOTES_AMOUNT) {
+        try {
+            println("\nВведите имя:")
+            val name = readln()
+
+            println("Введите номер телефона:")
+            val phoneNumber = readln().toLong()
+
+            println("Введите компанию:")
+            val company = readln().ifEmpty { null }
+
+            notes.add(Note4(name, phoneNumber, company))
+        } catch (e: NumberFormatException) {
+            println("Неверный ввод номера ${e.localizedMessage}")
+        }
+    }
+
+    notes.forEach {
+        it.print()
+        println()
+    }
+}
