5496f6cf656abc3214c853af116c34857f3303d0
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-17 18:25:26 +0000

    Сервер погоды

---
+++ src/main/kotlin/lesson15/lesson-15-task-2.kt
@@ -0,0 +1,24 @@
+package lesson15
+
+abstract class WeatherStationStats()
+
+class Temperature(val temperatureValue: Double): WeatherStationStats()
+
+class PrecipitationAmount(val precipitationAmount: Double): WeatherStationStats()
+
+class WeatherServer() {
+    fun sendWeatherMessage(stats: WeatherStationStats) {
+        when (stats) {
+            is Temperature -> println("Температура: ${stats.temperatureValue}")
+            is PrecipitationAmount -> println("Количество осадков: ${stats.precipitationAmount}")
+        }
+    }
+}
+
+fun main() {
+    val weatherServer = WeatherServer()
+
+    weatherServer.sendWeatherMessage(Temperature(30.toDouble()))
+    weatherServer.sendWeatherMessage(PrecipitationAmount(12.toDouble()))
+
+}
