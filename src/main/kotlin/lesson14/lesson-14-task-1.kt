c9a6fe867aa3f1dd3353a968d623bd2ac7159869
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-20 07:46:03 +0000

    Замечания

--- src/main/kotlin/lesson14/lesson-14-task-1.kt
+++ src/main/kotlin/lesson14/lesson-14-task-1.kt
@@ -1,33 +1,24 @@
 package lesson14
 
-const val LINEAR_SPEED = 10.toDouble()
-const val LINEAR_LOAD_CAPACITY = 10.toDouble()
-const val LINEAR_PASSENGERS_AMOUNT = 100
 
 open class LinearSheep(
-    val speed: Double = LINEAR_SPEED,
-    val loadCapacity: Double = LINEAR_LOAD_CAPACITY,
-    val passengersAmount: Int = LINEAR_PASSENGERS_AMOUNT,
+    val speed: Double = 10.toDouble(),
+    val loadCapacity: Double = 10.toDouble(),
+    val passengersAmount: Int = 100,
 )
 
-const val CARGO_SPEED = 5.toDouble()
-const val CARGO_LOAD_CAPACITY = 20.toDouble()
-const val CARGO_PASSENGERS_AMOUNT = 40
 
 class CargoSheep(
-    speed: Double = CARGO_SPEED,
-    loadCapacity: Double = CARGO_LOAD_CAPACITY,
-    passengersAmount: Int = CARGO_PASSENGERS_AMOUNT,
+    speed: Double = 5.toDouble(),
+    loadCapacity: Double = 20.toDouble(),
+    passengersAmount: Int = 40,
     ) : LinearSheep(speed, loadCapacity, passengersAmount)
 
-const val ICEBREAKER_SPEED = 3.toDouble()
-const val ICEBREAKER_LOAD_CAPACITY = 7.toDouble()
-const val ICEBREAKER_PASSENGERS_AMOUNT = 20
 
 class Icebreaker(
-    speed: Double = ICEBREAKER_SPEED,
-    loadCapacity: Double = ICEBREAKER_LOAD_CAPACITY,
-    passengersAmount: Int = ICEBREAKER_PASSENGERS_AMOUNT,
+    speed: Double = 3.toDouble(),
+    loadCapacity: Double = 7.toDouble(),
+    passengersAmount: Int = 20,
 ) : LinearSheep(speed, loadCapacity, passengersAmount) {
     val canBreakIce = true
 }
