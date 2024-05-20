f017e096ab2d022b4a2250181591f2ce5722ac90
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-20 14:02:54 +0000

    Корабли

---
+++ src/main/kotlin/lesson17/lesson-17-task-2.kt
@@ -0,0 +1,16 @@
+package lesson17
+
+class Ship(
+    name: String,
+    val averageSpeed: Double,
+    val homePort: String,
+) {
+    var name = name
+        get() = field
+        set(value) = println("Невозможно изменить имя корабля!")
+}
+
+fun main() {
+    val ship = Ship("Решительный", 100.toDouble(), "Владивосток")
+    ship.name = "Юрий Долгорукий"
+}
