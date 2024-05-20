3551ba66e7db0187ec269633fbc1d443f9a13c9d
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-20 13:58:20 +0000

    Игрок

---
+++ src/main/kotlin/lesson17/lesson-17-task-1.kt
@@ -0,0 +1,17 @@
+package lesson17
+
+import kotlin.reflect.KProperty
+
+class Question(
+    question: String,
+    answer: String,
+) {
+    val question = question
+        get() = field
+
+    var answer = answer
+        get() = field
+        set(value) {
+            field = value
+        }
+}
