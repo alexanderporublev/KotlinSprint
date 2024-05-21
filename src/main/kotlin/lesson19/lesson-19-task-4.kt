fb82d43532fa5bbad3b0f5b1d146769a0c4ac97b
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-21 12:49:07 +0000

    Танк

---
+++ src/main/kotlin/lesson19/lesson-19-task-4.kt
@@ -0,0 +1,40 @@
+package lesson19
+
+import kotlin.math.tan
+
+enum class BulletType {
+    Red{
+        override val power: Int
+            get() = 20
+    },
+    Blue{
+        override val power: Int
+            get() = 5
+    },
+    Green{
+        override val power: Int
+            get() = 10
+    };
+    abstract val power: Int
+}
+
+class Tank(
+
+) {
+    private var currentGunType: BulletType = BulletType.Red
+
+    fun changeGun(newGun: BulletType){
+        currentGunType = newGun
+    }
+
+    fun shut() = println("Наношу урон ${currentGunType.power}")
+}
+
+fun main() {
+    val tank = Tank()
+    tank.shut()
+    tank.changeGun(BulletType.Blue)
+    tank.shut()
+    tank.changeGun(BulletType.Green)
+    tank.shut()
+}
