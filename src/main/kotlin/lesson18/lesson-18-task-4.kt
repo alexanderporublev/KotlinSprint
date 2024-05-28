fa63a39fe2eb76c4ef3e74cc0278a0e293ee9962
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-28 13:41:50 +0000

    Замечания

--- src/main/kotlin/lesson18/lesson-18-task-4.kt
+++ src/main/kotlin/lesson18/lesson-18-task-4.kt
@@ -1,28 +1,25 @@
 package lesson18
 
-import sun.security.util.Length
 
-interface Surface{
-    val surfaceSquare: Double
+interface Surface {
+    fun surfaceSquare(): Double
 }
 
 class Cube(
     val ribLength: Double,
-) : Surface{
-    override val surfaceSquare: Double
-        get() = 6 * ribLength * ribLength
+) : Surface {
+    override fun surfaceSquare(): Double = 6 * ribLength * ribLength
 }
 
 class Parallelepiped(
     val length: Double,
     val width: Double,
     val height: Double,
-): Surface{
-    override val surfaceSquare: Double
-        get() = 2 * (length * width + length * height + height * width)
+) : Surface {
+    override fun surfaceSquare(): Double = 2 * (length * width + length * height + height * width)
 }
 
 fun main() {
-    println("Площадь поверхности куба: ${Cube(5.0).surfaceSquare}")
-    println("Площадь поверхности параллелепипеда: ${Parallelepiped(5.0, 3.0, 8.0).surfaceSquare}")
+    println("Площадь поверхности куба: ${Cube(5.0).surfaceSquare()}")
+    println("Площадь поверхности параллелепипеда: ${Parallelepiped(5.0, 3.0, 8.0).surfaceSquare()}")
 }
