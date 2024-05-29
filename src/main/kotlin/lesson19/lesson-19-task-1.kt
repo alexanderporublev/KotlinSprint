b7250eb691ab6c436ba8d489ed250717d196c031
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-05-29 19:44:05 +0000

    Замечания

--- src/main/kotlin/lesson19/lesson-19-task-1.kt
+++ src/main/kotlin/lesson19/lesson-19-task-1.kt
@@ -1,7 +1,12 @@
 package lesson19
 
-enum class Fish {Guppy, Angelfish, Goldfish, SiameseFightingFish}
+enum class Fish(var nameFish: String) {
+    Guppy("Гуппи"),
+    Angelfish("Скалярия"),
+    Goldfish("Золотая рыбка"),
+    SiameseFightingFish("Петушок"),
+}
 
 fun main() {
-    println("Вы можете добавить следующие виды рыб ${Fish.entries.joinToString()}")
+    println("Вы можете добавить следующие виды рыб ${Fish.entries.map { it.nameFish }.joinToString()}")
 }
